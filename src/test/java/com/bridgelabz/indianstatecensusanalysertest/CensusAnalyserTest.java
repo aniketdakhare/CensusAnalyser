7a376165dd4722c17e7341c0a50795005a16f082
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-29 17:36:00 +0530

    ADDED Test method to whether given Indian Census CSV file returns correct records or not

---
+++ src/test/java/com/bridgelabz/indianstatecensusanalysertest/CensusAnalyserTest.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.indianstatecensusanalysertest;
+
+import com.bridgelabz.indianstatecensusanalyser.exception.CensusAnalyserException;
+import com.bridgelabz.indianstatecensusanalyser.services.IndianStateCensusAnalyser;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class CensusAnalyserTest
+{
+    private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+    private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+
+    @Test
+    public void givenIndianCensusCSVFile_ReturnsCorrectRecords()
+    {
+        try
+        {
+            IndianStateCensusAnalyser censusAnalyser = new IndianStateCensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            Assert.assertEquals(29,numOfRecords);
+        }
+        catch (CensusAnalyserException ignored)
+        { }
+    }
+}
