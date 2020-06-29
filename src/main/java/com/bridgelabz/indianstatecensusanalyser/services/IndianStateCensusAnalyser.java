a4951aed5dde2b922c77325294bd84411a90b2cf
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-29 17:32:33 +0530

    ADDED IndianStateCensusAnalyser class to load the information from corresponding csv file

---
+++ src/main/java/com/bridgelabz/indianstatecensusanalyser/services/IndianStateCensusAnalyser.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.indianstatecensusanalyser.services;
+
+import com.bridgelabz.indianstatecensusanalyser.exception.CensusAnalyserException;
+import com.bridgelabz.indianstatecensusanalyser.model.IndiaCensusCSV;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+public class IndianStateCensusAnalyser
+{
+    public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException
+    {
+        try
+        {
+            Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+            CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(IndiaCensusCSV.class);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
+            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
+            int namOfEateries = 0;
+            while (censusCSVIterator.hasNext())
+            {
+                namOfEateries++;
+                IndiaCensusCSV censusData = censusCSVIterator.next();
+            }
+            return namOfEateries;
+        }
+        catch (IOException e)
+        {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        }
+    }
+}
