a1503fab59c2398024d8458ccef9783619176cec
“Aniket_D” <aniketdakhare@gmail.com>
2020-07-01 21:14:55 +0530

    ADDED Common CSV and implemented it

--- src/main/java/com/bridgelabz/indianstatecensusanalyser/services/IndianStateCensusAndCodeAnalyser.java
+++ src/main/java/com/bridgelabz/indianstatecensusanalyser/services/IndianStateCensusAndCodeAnalyser.java
@@ -18,6 +18,7 @@ public class IndianStateCensusAndCodeAnalyser
 {
     /**
      * METHOD TO LOAD INDIAN STATE CENSUS DATA
+     * Note:- Pass argument as '0' for OpenCSV and '1' for CommonCSV in createCSVBuilder method
      * @param csvFilePath provides the path of file
      * @param separator provides the seperator for records in csv file
      * @return number of records
@@ -27,7 +28,7 @@ public class IndianStateCensusAndCodeAnalyser
     {
         try( Reader reader = Files.newBufferedReader(Paths.get(csvFilePath)))
         {
-            Iterator<IndiaCensusCSV> censusCSVIterator = CSVBuilderFactory.createCSVBuilder()
+            Iterator<IndiaCensusCSV> censusCSVIterator = CSVBuilderFactory.createCSVBuilder(1)
                     .getCSVFileIterator(reader, IndiaCensusCSV.class, separator);
             return this.getCount(censusCSVIterator);
         }
@@ -41,6 +42,11 @@ public class IndianStateCensusAndCodeAnalyser
             throw new IndianStateCensusAndCodeAnalyserException(e.getMessage(),
                     IndianStateCensusAndCodeAnalyserException.ExceptionType.CSV_FILE_PROBLEM);
         }
+        catch (RuntimeException e)
+        {
+            throw new IndianStateCensusAndCodeAnalyserException("Entered incorrect Delimiter or incorrect Header",
+                    IndianStateCensusAndCodeAnalyserException.ExceptionType.INCORRECT_DELIMITER_OR_HEADER);
+        }
         catch (CSVBuilderException e)
         {
             throw new IndianStateCensusAndCodeAnalyserException(e.getMessage(), e.type.name());
@@ -49,6 +55,7 @@ public class IndianStateCensusAndCodeAnalyser
 
     /**
      * METHOD TO LOAD INDIAN STATE CENSUS DATA
+     * Note:- Pass argument as '0' for OpenCSV and '1' for CommonCSV in createCSVBuilder method
      * @param csvFilePath provides the path of file
      * @param separator provides the seperator for records in csv file
      * @return number of records
@@ -58,7 +65,7 @@ public class IndianStateCensusAndCodeAnalyser
     {
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath)))
         {
-            Iterator<IndiaStateCodeCSV> codeCSVIterator = CSVBuilderFactory.createCSVBuilder()
+            Iterator<IndiaStateCodeCSV> codeCSVIterator = CSVBuilderFactory.createCSVBuilder(1)
                     .getCSVFileIterator(reader, IndiaStateCodeCSV.class, separator);
             return this.getCount(codeCSVIterator);
         }
@@ -71,6 +78,11 @@ public class IndianStateCensusAndCodeAnalyser
         {
             throw new IndianStateCensusAndCodeAnalyserException(e.getMessage(),
                     IndianStateCensusAndCodeAnalyserException.ExceptionType.CSV_FILE_PROBLEM);
+        }
+        catch (RuntimeException e)
+        {
+            throw new IndianStateCensusAndCodeAnalyserException("Entered incorrect Delimiter or incorrect Header",
+                    IndianStateCensusAndCodeAnalyserException.ExceptionType.INCORRECT_DELIMITER_OR_HEADER);
         }
         catch (CSVBuilderException e)
         {
@@ -88,6 +100,7 @@ public class IndianStateCensusAndCodeAnalyser
     {
         Iterable<E> codeData = () -> iterator;
         int numberOfEntries = (int) StreamSupport.stream(codeData.spliterator(), false).count();
+        System.out.println(numberOfEntries);
         return numberOfEntries;
     }
 }
