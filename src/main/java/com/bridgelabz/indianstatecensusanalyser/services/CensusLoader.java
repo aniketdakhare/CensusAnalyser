e2270c0a34c136b60b4dd5f3da2f5d87eac0619e
“Aniket_D” <aniketdakhare@gmail.com>
2020-07-06 01:32:33 +0530

    REFACTORED Code in order to achieve DRY principle

--- src/main/java/com/bridgelabz/indianstatecensusanalyser/services/CensusLoader.java
+++ src/main/java/com/bridgelabz/indianstatecensusanalyser/services/CensusLoader.java
@@ -63,6 +63,18 @@ public class CensusLoader
                     StreamSupport.stream(csvIterable.spliterator(), false)
                             .map(IndiaCensusCSV.class::cast)
                             .forEach(csvState -> censusMap.put(csvState.state, new CensusDAO(csvState)));
+                    if (csvFilePath.length == 2)
+                    {
+                        try (Reader codeReader = Files.newBufferedReader(Paths.get(csvFilePath[1])))
+                        {
+                            Iterator<IndiaStateCodeCSV> stateCodeIterator = CSVBuilderFactory.createCSVBuilder(0)
+                                    .getCSVFileIterator(codeReader, IndiaStateCodeCSV.class, separator);
+                            Iterable<IndiaStateCodeCSV> codeCSVIterable = () -> stateCodeIterator;
+                            StreamSupport.stream(codeCSVIterable.spliterator(), false)
+                                    .filter(csvState -> censusMap.get(csvState.StateName) != null)
+                                    .forEach(csvState -> censusMap.get(csvState.StateName).stateCode = csvState.StateCode);
+                        }
+                    }
                     break;
                 case "USCensusCSV":
                     StreamSupport.stream(csvIterable.spliterator(), false)
@@ -70,50 +82,7 @@ public class CensusLoader
                             .forEach(csvState -> censusMap.put(csvState.state, new CensusDAO(csvState)));
                     break;
             }
-            if (csvFilePath.length == 1) return censusMap;
-            loadIndiaStateCode(csvFilePath[1], separator, censusMap);
             return censusMap;
-        }
-        catch (NoSuchFileException e)
-        {
-            throw new CensusAnalyserException("Entered wrong file name/path or wrong file extension",
-                    CensusAnalyserException.ExceptionType.CSV_FILE_PROBLEM);
-        }
-        catch (IOException e)
-        {
-            throw new CensusAnalyserException(e.getMessage(),
-                    CensusAnalyserException.ExceptionType.CSV_FILE_PROBLEM);
-        }
-        catch (RuntimeException e)
-        {
-            throw new CensusAnalyserException("Entered incorrect Delimiter or incorrect Header",
-                    CensusAnalyserException.ExceptionType.INCORRECT_DELIMITER_OR_HEADER);
-        }
-        catch (CSVBuilderException e)
-        {
-            throw new CensusAnalyserException(e.getMessage(), e.type.name());
-        }
-    }
-
-    /**
-     * METHOD TO LOAD INDIAN STATE CODE DATA
-     * Note:- Pass argument as '0' for OpenCSV and '1' for CommonCSV in createCSVBuilder method
-     * @param csvFilePath provides the path of file
-     * @param separator provides the seperator for records in csv file
-     * @param censusMap provides the Indian census map
-     * @throws CensusAnalyserException while handling the occurred exception
-     */
-    private void loadIndiaStateCode(String csvFilePath, char separator, Map<String, CensusDAO> censusMap)
-            throws CensusAnalyserException
-    {
-        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath)))
-        {
-            Iterator<IndiaStateCodeCSV> stateCodeIterator = CSVBuilderFactory.createCSVBuilder(0)
-                    .getCSVFileIterator(reader, IndiaStateCodeCSV.class, separator);
-            Iterable<IndiaStateCodeCSV> csvIterable = () -> stateCodeIterator;
-            StreamSupport.stream(csvIterable.spliterator(), false)
-                    .filter(csvState -> censusMap.get(csvState.StateName) != null)
-                    .forEach(csvState -> censusMap.get(csvState.StateName).stateCode = csvState.StateCode);
         }
         catch (NoSuchFileException e)
         {
